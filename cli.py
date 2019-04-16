5fb38d0f3c92a84f9b99c2f8bd8372a453c2d63c
Madhukant <kantmadhu956@gmail.com>
2019-04-16 14:52:38 +0530

    cli

--- cli.py
+++ cli.py
@@ -5,6 +5,7 @@ import sqlite3
 
 from User import User
 from contextlib import closing
+from termcolor import colored
 
 
 name = "Cindy"
@@ -29,14 +30,14 @@ while True:
 			name = input()
 			if name == "":
 				name = "Alice Garcia"
-			print("Hello "+name)
+			print("Hello "+colored(name, 'blue'))
 			print("Please give your wallet file path: ", end = "")
 			wallet_file = input()
 			if wallet_file == "":
 				wallet_file = "wallet.yaml"
 			print("Enter your ssn: ", end = "")
 			ssn = int(input())
-			print(ssn)
+			print("Your SSN:" + colored(ssn, 'blue'))
 			try:
 				user = User(name = name, wallet_file = wallet_file, ssn = ssn)
 				print("Wallet Loaded")
@@ -52,30 +53,30 @@ while True:
 				continue
 			print("Certificates available: " + str(user.wallet.keys()))
 			for cert_name in user.wallet.keys():
-				print ("********* CERTIFICATE: {} ***********".format(cert_name))
+				print ("*********" + colored('CERTIFICATE', 'red') + ": "+ colored("{}".format(cert_name), 'yellow')+" ***********\n")
 				cert_file = user.wallet[cert_name]
 				cert = yaml.load(open(cert_file))
 				for attr in cert.keys():
 					if attr != 'Attributes':
-						print(attr + " : " + str(cert[attr]))
+						print(colored(attr, 'blue') + " : " + colored(str(cert[attr]), 'green'))
 					else:
 						for ind_attr in cert[attr]:
-							print('\t'+ind_attr+' : '+str(cert[attr][ind_attr]))
-				print("*************************************************\n")
+							print('\t'+colored(ind_attr, 'blue')+' : '+ colored(str(cert[attr][ind_attr]), 'green'))
+				print("\n*************************************************\n")
 		if inp_list[1] in ['certificate', 'cert']:
 			print("Certificates available: " + str(user.wallet.keys()))
 			print("Which one do you want to view? : ", end = "")
 			cert_name = input()
-			print ("********* CERTIFICATE: {} ***********".format(cert_name))
+			print ("*********" + colored('CERTIFICATE', 'red') + ": "+ colored("{}".format(cert_name), 'yellow')+" ***********\n")
 			cert_file = user.wallet[cert_name]
 			cert = yaml.load(open(cert_file))
 			for attr in cert.keys():
 				if attr != 'Attributes':
-					print(attr + " : " + str(cert[attr]))
+					print(colored(attr, 'blue') + " : " + colored(str(cert[attr]), 'green'))
 				else:
 					for ind_attr in cert[attr]:
-						print('\t'+ind_attr+' : '+str(cert[attr][ind_attr]))
-			print("*************************************************\n")
+						print('\t'+colored(ind_attr, 'blue') +' : '+ colored(str(cert[attr][ind_attr])), 'green')
+			print("\n*************************************************\n")
 
 
 	if inp_list[0] in ['set']:
@@ -84,7 +85,6 @@ while True:
 			company = input()
 			if company == "":
 				company = "ABC University"
-
 			with closing(sqlite3.connect('GlobalVs.db')) as con, con, closing(con.cursor()) as c:
 				query = """SELECT url FROM GlobalVs WHERE InstituteName = '{}';""".format(company)
 				c.execute(query)
@@ -92,7 +92,7 @@ while True:
 				company_url = result[0][0]
 			# company_url = globalVs['url'][company]
 			schema = requests.get(company_url+'cert_schema').json()
-			print("Issuer set as "+ company +" with url " + company_url + " and following schema: ")
+			print("Issuer set as "+ colored(company, 'blue') +" with url " + colored(company_url, 'blue') + " and following schema: ")
 			print(schema)
 
 
@@ -103,15 +103,15 @@ while True:
 				values = {}
 				if schema['Proof_Request']:
 					for attr in schema['Proof_Request']:
-						print("Enter "+ str(attr) + " value: ", end = "")
+						print("Enter "+ colored(str(attr), 'red') + " value: ", end = "")
 						values[attr] = input()
 				user.requestCertificate(issuer = company, values = values)
 				print("Obtained certificate, stored in wallet.")
 
 			if len(inp_list) > 2 and inp_list[2] in ['name']:
-				print("Certificate name: "+str(requests.get(company_url+'cert_name').json()['cert_name']))
+				print("Certificate name: "+colored(str(requests.get(company_url+'cert_name').json()['cert_name']), 'blue'))
 			if len(inp_list) > 2 and inp_list[2] in ['public_key', 'pkey']:
-				print("Public key for Issuer: "+str(requests.get(company_url+'pkey').json()['pkey']))
+				print("Public key for Issuer: "+colored(str(requests.get(company_url+'pkey').json()['pkey']), 'blue'))
 
 
 
